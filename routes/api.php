5639784cc6c6ec6a35a257b8e0494d6873a82e51
Avinash Singh <avinashs@prtouch.com>
2021-02-04 14:53:14 +0530

    Add APIs

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('students', 'App\Http\Controllers\ApiController@getAllStudents');
+Route::get('students/{id}', 'App\Http\Controllers\ApiController@getStudent');
+Route::post('students/create', 'App\Http\Controllers\ApiController@createStudent');
+Route::put('students/update/{id}', 'App\Http\Controllers\ApiController@updateStudent');
+Route::delete('students/delete/{id}','App\Http\Controllers\ApiController@deleteStudent');
